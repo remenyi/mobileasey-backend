b2b862716ab8a5cc04c468e726b3aea4299a85cd
Gergely Reményi <gergo@gergo.city>
2021-11-28 17:42:15 +0100

    Fix equipmentID - equipmentId typo

--- middleware/equipmentMWs/getAllEquipmentMW.js
+++ middleware/equipmentMWs/getAllEquipmentMW.js
@@ -3,7 +3,7 @@ const Equipment = require("../../models/device");
 module.exports = async function(req, res, next) {
     Equipment.find({}, (err, equipments) => {
         if (err) return res.sendStatus(404);
-        equipmentsDTO = equipments.map(e => {return {name: e.name, equipmentID: e._id, equipmentType: e.type}})
+        equipmentsDTO = equipments.map(e => {return {name: e.name, equipmentId: e._id, equipmentType: e.type}})
         res.send(equipmentsDTO);
         next();
     });
